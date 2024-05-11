d73b77846b3bbb0ee44de8ad3a362d3dac906352
OJO65 <dmallowa@gmail.com>
2024-05-11 10:02:10 +0300

    logo & search bar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from 'react';
+import logo from "../Assets/img/logo.png";
 
 const Navbar = () => {
   return (
-    <div>
-      
+    <div className="flex items-center">
+      <div className="mr-4">
+        <img src={logo} className='w-40' alt="Logo" />
+      </div>
+      <div>
+        <input type="text" placeholder="Search..." className="border border-gray-300 px-3 py-1 rounded-lg" />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
