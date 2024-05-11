bd4cc7bb8a39ccdd75d17f7da6db4fee85610175
OJO65 <dmallowa@gmail.com>
2024-05-11 11:17:00 +0300

    fontawesome icons

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,17 +1,32 @@
-import React from 'react';
+import React from "react";
 import logo from "../Assets/img/logo.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
   return (
     <div className="flex items-center">
       <div className="mr-4">
-        <img src={logo} className='w-40' alt="Logo" />
+        <img src={logo} className="w-40" alt="Logo" />
       </div>
-      <div>
-        <input type="text" placeholder="Search..." className="border border-gray-300 px-3 py-1 rounded-lg" />
+      <div className="flex">
+        <div className="relative">
+          <input
+            type="text"
+            placeholder="search products, brands and categories"
+            className="border border-gray-300 px-3 py-1 rounded w-96 mr-2 pl-8 text-lg"
+          />
+          <FontAwesomeIcon
+            icon={faSearch}
+            className="absolute left-2 top-3 text-gray-400"
+          />
+        </div>
+        <button className="border border-gray-300 px-3 py-1 rounded bg-orange-400 text-white font-bold">
+          SEARCH
+        </button>
       </div>
     </div>
   );
-}
+};
 
 export default Navbar;
