f53d14985a3bb0c4ae0496fd924e1314df09f12a
OJO65 <dmallowa@gmail.com>
2024-05-13 23:20:37 +0300

    navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import logo from "../Assets/img/logo.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSearch, faUser } from "@fortawesome/free-solid-svg-icons";
+import { faSearch, faUser, faCircleQuestion, faCartShopping } from "@fortawesome/free-solid-svg-icons";
 const Navbar = () => {
   return (
-    <div className="flex items-center">
-      <div className="mr-4">
+    <>
+    <div className="flex items-center box-border shadow-md">
+      <div className=" mr-32 ml-32">
         <img src={logo} className="w-40" alt="Logo" />
       </div>
       <div className="flex">
@@ -24,12 +25,28 @@ const Navbar = () => {
           SEARCH
         </button>
         <div className="flex">
+          <div className="dropdown">
           <FontAwesomeIcon icon={faUser} 
           className="w-10 h-5"
           />
+          <button type="" className="border p-2 ">Account</button>
+          </div>
+          <div className="dropdown">
+          <FontAwesomeIcon icon={faCircleQuestion} 
+          className="w-10 h-5"
+          />
+          <button type="" className="border p-2 ">Help</button>
+          </div>
+          <div className="dropdown">
+          <FontAwesomeIcon icon={faCartShopping} 
+          className="w-10 h-5"
+          />
+          <button type="" className="border p-2 ">Cart</button>
+          </div>
         </div>
       </div>
     </div>
+    </>
   );
 };
 
