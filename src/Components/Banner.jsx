b8853b38e70a29f74388154c80eeb8f9b9be2305
OJO65 <dmallowa@gmail.com>
2024-05-26 16:51:04 +0300

    gif container in the bag

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -1,5 +1,6 @@
 import { faAppleWhole, faBaby, faBasketball, faBlenderPhone, faComputer, faHome, faListDots, faMedkit, faMobilePhone, faShirt, faShoppingBag, faTelevision } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useState, useEffect } from 'react';
 import "../Assets/img/KE_FS_NewArrivals_0524_S.gif";
 import gif1 from '../Assets/img/KE_FS_NewArrivals_0524_S.gif';
 import gif2 from '../Assets/img/KE_Generic_JA24_Teasing_0524_S.gif';
@@ -9,7 +10,6 @@ import gif5 from '../Assets/img/KE_FS_NewArrivals_0524_S.gif';
 import gif6 from '../Assets/img/KE_JA24_Tease_ACD_0524_S3.gif';
 import gif7 from '../Assets/img/KE_JA24_Vote_0524_S.gif';
 import gif8 from '../Assets/img/_S_ecobank.jpg';
-import React, { useState, useEffect } from 'react';
 
 const Banner = () => {
   const gifs = [gif1, gif2, gif3, gif4, gif5, gif6, gif7, gif8];
@@ -18,15 +18,19 @@ const Banner = () => {
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentGifIndex(prevIndex => (prevIndex + 1) % gifs.length);
-    }, 1000);
+    }, 5000);
 
     return () => clearInterval(interval);
   }, [gifs.length]);
 
+  const handleDotClick = (index) => {
+    setCurrentGifIndex(index);
+  };
+
   return (
     <div className='flex m-2'>
-      <div className='border w-1/6 m-2 p-2 text-sm rounded-md bg-white'>
-        <ul className='p-2'>
+      <div className='border w-1/6 m-2 p-2 text-sm rounded-md bg-white h-[384px]'>
+        <ul className='p-2 h-full overflow-auto'>
           <li className='py-2'><FontAwesomeIcon icon={faShoppingBag} className='mr-2'/> Official Stores</li>
           <li className='py-2'><FontAwesomeIcon icon={faMobilePhone} className='mr-2'/> Phones & Tablets</li>
           <li className='py-2'><FontAwesomeIcon icon={faTelevision} className='mr-2'/> Tvs & Audio</li>
@@ -42,8 +46,17 @@ const Banner = () => {
         </ul>
       </div>
 
-      <div className='m-2 border flex-grow-0 flex-shrink-0 w-auto h-auto overflow-hidden flex items-center justify-center' style={{ height: 'calc(100vh - 4rem)' }}>
+      <div className='m-2 border rounded-md flex-grow-0 flex-shrink-0 w-[712px] h-[384px] relative overflow-hidden flex items-center justify-center'>
         <img src={gifs[currentGifIndex]} alt={`GIF ${currentGifIndex + 1}`} className='w-full h-full object-contain' />
+        <div className="absolute bottom-4 left-0 right-0 flex justify-center">
+          {gifs.map((gif, index) => (
+            <span
+              key={index}
+              onClick={() => handleDotClick(index)}
+              className={`w-2 h-2 rounded-full mx-1 cursor-pointer ${index === currentGifIndex ? 'bg-orange-500' : 'bg-gray-300'}`}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
