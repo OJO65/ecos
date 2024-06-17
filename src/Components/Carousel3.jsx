f97bce52fe41d4bd54791f3782ca8ef356525e62
OJO65 <dmallowa@gmail.com>
2024-06-17 12:49:10 +0300

    carousel3

---
+++ src/Components/Carousel3.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import blender from '../Assets/img/flash/blender.jpg';
+import menengai from '../Assets/img/flash/menengai.jpg';
+import dettol from '../Assets/img/flash/dettol.jpg';
+import dettol2 from '../Assets/img/flash/dettol2.jpg';
+import vaseline from '../Assets/img/flash/vaseline.jpg';
+import Harpic from '../Assets/img/flash/harpic.jpg';
+import menengai from '../Assets/img/flash/menengai.jpg';
+import menengai from '../Assets/img/flash/menengai.jpg';
+import menengai from '../Assets/img/flash/menengai.jpg';
+import menengai from '../Assets/img/flash/menengai.jpg';
+import menengai from '../Assets/img/flash/menengai.jpg';
+import menengai from '../Assets/img/flash/menengai.jpg';
+import menengai from '../Assets/img/flash/menengai.jpg';
+import menengai from '../Assets/img/flash/menengai.jpg';
+import menengai from '../Assets/img/flash/menengai.jpg';
+
+const Carousel3 = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Carousel3;
