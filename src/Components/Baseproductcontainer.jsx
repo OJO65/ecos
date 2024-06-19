fb3cbc5777af70f8b0775672a5e30220312cd6ce
OJO65 <dmallowa@gmail.com>
2024-06-19 14:36:52 +0300

    product container done

--- src/Components/Baseproductcontainer.jsx
+++ src/Components/Baseproductcontainer.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 const Baseproductcontainer = ({ items, itemRenderer }) => {
   return (
-    <div>
-      <header className='bg-gray-500'>
-        <h1>Product Container</h1>
+    <div className='bg-white ml-[20px] mr-[20px] rounded-lg'>
+      <header className='bg-blue-500 text-lg font-semibold text-center p-3'>
+        <h1>Top Deals For You | Shop Now</h1>
       </header>
-      <div>
+      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-4 p-2'>
         {items.map((item, index) => (
           <div key={index}>
             {itemRenderer(item)}
