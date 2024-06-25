80b015c946fc2288a6576ce9da46d1f3ca7ce24a
OJO65 <dmallowa@gmail.com>
2024-06-25 23:11:09 +0300

    footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import logo from "../Assets/img/logo2.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {faAppleAlt, faEnvelope} from "@fortawesome/free-solid-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import google from "../Assets/img/google-play.svg";
 import apple from "../Assets/img/apple.svg";
 import star from "../Assets/img/star.png";
+import {
+  faFacebook,
+  faInstagram,
+  faYoutube,
+} from "@fortawesome/free-brands-svg-icons";
 const Footer = () => {
   return (
     <>
@@ -31,7 +36,7 @@ const Footer = () => {
               className="relative text-gray-400 h-[20px] w-[20px] right-56 translate-y-1"
             />
 
-            <div className="w-[240px] ml-[250px] -translate-y-16">
+            <div className="w-[240px] ml-[250px] -translate-y-16 -translate-x-5">
               <button className="border hover:text-orange-500 hover:border-orange-500 p-2 bg-gray-900 h-[50px] w-[100px] m-2 rounded-md font-bold text-white">
                 MALE
               </button>
@@ -53,50 +58,101 @@ const Footer = () => {
         </a>
         <div className="flex-row translate-x-[950px] -translate-y-48">
           <div className="m-2">
-            <img src={star} className="h-[50px] ml-3"/>
+            <img src={star} className="h-[50px] ml-3" />
           </div>
           <h1 className="text-white font-bold translate-x-20 -translate-y-16">
             DOWNLOAD JUMIA FREE APP
           </h1>
           <p className="text-white text-sm translate-x-20 -translate-y-16">
             Get access to exclusive offers!
-            
           </p>
           <div className="flex flex-row w-52 -mt-16 ml-20">
             <div className="m-1">
-          <p className="text-white text-xs">Download on the</p>
-          <img src={apple} className="h-[20px] -ml-5 -mt-5 translate-y-5"/>
-          <a href="" className="text-white font-semibold">App Store</a>
+              <p className="text-white text-xs">Download on the</p>
+              <img src={apple} className="h-[20px] -ml-5 -mt-5 translate-y-5" />
+              <a href="" className="text-white font-semibold">
+                App Store
+              </a>
+            </div>
+            <div className="translate-x-8 p-1">
+              <p className="text-white text-xs">Download on the</p>
+              <img
+                src={google}
+                className="h-[20px] -ml-6 -mt-5 translate-y-5"
+              />
+              <a href="" className="text-white font-semibold">
+                Google Play
+              </a>
+            </div>
           </div>
-          <div className="translate-x-8 p-1">
-          <p className="text-white text-xs">Download on the</p>
-          <img src={google} className="h-[20px] -ml-6 -mt-5 translate-y-5"/>
-          <a href="" className="text-white font-semibold">Google Play</a>
-          </div>
-          </div> 
         </div>
       </div>
-      <div className="bg-gray-500 h-[50vh] -translate-y-20">
-          <p className="text-white font-bold text-sm ml-5 p-2">NEED HELP?</p>
-          <ul className="text-white text-xs ml-7">
-            <li>Chat with us</li>
-            <li>Help Center</li>
-            <li>Contact Us</li>
-          </ul>
+      <div className="bg-gray-500 h-[80vh] -translate-y-20">
+        <div className="grid grid-cols-6 border translate-y-3">
+          <div>
+            <p className="text-white font-bold text-sm ml-5 p-2">NEED HELP?</p>
+            <ul className="text-white text-xs ml-7">
+              <li>Chat with us</li>
+              <li>Help Center</li>
+              <li>Contact Us</li>
+            </ul>
+          </div>
           <br />
-          <p className="text-white font-bold text-sm ml-5 p-2">USEFUL LINKS</p>
-          <ul className="text-white text-xs ml-7">
-            <li>Track Your Order</li>
-            <li>Shipping and Delivery</li>
-            <li>Pick-up Stations</li>
-            <li>Return Policy</li>
-            <li>How To Order?</li>
-            <li>Dispute Resolution Policy</li>
-            <li>Coperate and Bulk Purchase</li>
-            <li>Advertise with Jumia</li>
-            <li>Report a Product</li>
-            <li>Jumia Payment Information Guidelines</li>
-          </ul>
+          <div>
+            <p className="text-white font-bold text-sm ml-5 p-2">
+              USEFUL LINKS
+            </p>
+            <ul className="text-white text-xs ml-7">
+              <li>Track Your Order</li>
+              <li>Shipping and Delivery</li>
+              <li>Pick-up Stations</li>
+              <li>Return Policy</li>
+              <li>How To Order?</li>
+              <li>Dispute Resolution Policy</li>
+              <li>Coperate and Bulk Purchase</li>
+              <li>Advertise with Jumia</li>
+              <li>Report a Product</li>
+              <li>Jumia Payment Information Guidelines</li>
+            </ul>
+          </div>
+          <br />
+          <div>
+            <p className="text-white font-bold text-sm ml-5 p-2">ABOUT JUMIA</p>
+            <ul className="text-white text-xs ml-7">
+              <li>About us</li>
+              <li>Returns and Refunds Policy</li>
+              <li>Jumia Careers</li>
+              <li>Jumia Express</li>
+              <li>Terms & Conditions</li>
+              <li>Store Credit Terms & Conditions</li>
+              <li>Privacy Notice</li>
+              <li>Cookies Notice</li>
+              <li>Flash Sales</li>
+              <li>JUmia Global</li>
+              <li>Jumia anniversary 2024</li>
+            </ul>
+          </div>
+          <br />
+          <div>
+            <p className="text-white font-bold text-sm ml-5 p-2">
+              MAKE MONEY WITH JUMIA
+            </p>
+            <ul className="text-white text-xs ml-7">
+              <li>Sell on Jumia</li>
+              <li>Vendor Hub</li>
+              <li>Become a Sales Consultant</li>
+              <li>Become a Logistics Service Partner</li>
+              <li>Jumia City Partner Program</li>
+            </ul>
+          </div>
+        </div>
+        <br />
+        <p className="text-white text-sm font-semibold ml-7">JOIN US</p>
+        <div className="flex ml-6">
+          <FontAwesomeIcon icon={faFacebook} className="m-1" />
+          <FontAwesomeIcon icon={faInstagram} className="m-1" />
+          <FontAwesomeIcon icon={faYoutube} className="m-1" />
+        </div>
       </div>
     </>
   );
