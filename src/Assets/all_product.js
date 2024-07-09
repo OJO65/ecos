709d81c6d23d4b205351f6a623164ca291bb2a9b
OJO65 <dmallowa@gmail.com>
2024-07-09 23:37:42 +0300

    "data for phones"

--- src/Assets/all_product.js
+++ src/Assets/all_product.js
@@ -1,16 +1,34 @@
 import infinix from "../Assets/img/Phones & Tablets/Infinix Note 40.jpg";
-
-
+import infinix2 from "../Assets/img/Phones & Tablets/Infinix Note 40i.jpg";
+import Samsung from "../Assets/img/Phones & Tablets/Samsung Galaxy A05s.jpg";
 
 
 const products = [
     {
         id: 1,
         category: "Phones & Tablets",
-        image: "infinix",
+        image: infinix,
         name: "Infinix Note 40",
         newPrice: "Ksh 25,999",
         oldPrice: "Ksh 43,999",
         discount: "-40%"
-    }
+    },
+    {
+        id: 2,
+        category: "Phones & Tablets",
+        image: infinix2,
+        name: "Infinix Note 40i",
+        newPrice: "Ksh 11,838",
+        oldPrice: "Ksh 14,000",
+        discount: "-15%"
+    },
+    {
+        id: 3,
+        category: "Phones & Tablets",
+        image: Samsung,
+        name: "Samsung Galaxy A05s",
+        newPrice: "Ksh 11,946",
+        oldPrice: "Ksh 14,000",
+        discount: "-15%"
+    },
 ]
