a85a95531cbcd06aef4f8ab822f84ded1b97e0f3
OJO65 <dmallowa@gmail.com>
2024-07-10 22:20:56 +0300

    "phone data"

--- src/Assets/all_product.js
+++ src/Assets/all_product.js
@@ -1,11 +1,14 @@
 import infinix from "../Assets/img/Phones & Tablets/Infinix Note 40.jpg";
 import infinix2 from "../Assets/img/Phones & Tablets/Infinix Note 40i.jpg";
 import Samsung from "../Assets/img/Phones & Tablets/Samsung Galaxy A05s.jpg";
-
+import Itel from "../Assets/img/Phones & Tablets/itel S23+.jpg";
+import Samsung2 from "../Assets/img/Phones & Tablets/Samsung galaxy A04e.jpg";
+import Tecno from "../Assets/img/Phones & Tablets/Tecno CAMON.jpg";
+import Redmi from "../Assets/img/Phones & Tablets/Xiaomi Redmi Note 13.jpg";
 
 const products = [
     {
-        id: 1,
+        id: 1, 
         category: "Phones & Tablets",
         image: infinix,
         name: "Infinix Note 40",
@@ -31,4 +34,42 @@ const products = [
         oldPrice: "Ksh 14,000",
         discount: "-15%"
     },
-]
+    {
+        id: 4,
+        category: "Phones & Tablets",
+        image: Itel,
+        name: "Itel S23+",
+        newPrice: "Ksh 20,315",
+        oldPrice: "Ksh 23,000",
+        discount: "-12%"
+    },
+    {
+        id: 5,
+        category: "Phones & Tablets",
+        image: Samsung2,
+        name: "Samsung Galaxy A04e",
+        newPrice: "Ksh 11,946",
+        oldPrice: "Ksh 14,000",
+        discount: "-15%"
+    },
+    {
+        id: 6,
+        category: "Phones & Tablets",
+        image: Tecno,
+        name: "Tecno CAMON",
+        newPrice: "Ksh 11,838",
+        oldPrice: "Ksh 14,000",
+        discount: "-15%"
+    },
+    {
+        id: 7,
+        category: "Phones & Tablets",
+        image: Redmi,
+        name: "Xiaomi Redmi Note 13",
+        newPrice: "Ksh 19,306",
+        oldPrice: "Ksh 21,000",
+        discount: "-8%"
+    },
+];
+
+export default products;
